c660e7d9c5a01548db32a9335eafa626455ffeb8
Pedro Carvalho <pedroboh@live.com>
2021-06-29 22:46:13 -0300

    ready to release

--- js/main.js
+++ js/main.js
@@ -308,3 +308,58 @@ function incrementValue(id)
     value++;
     document.getElementById(id).innerHTML = value;
 }
+
+/* globals Chart:false, feather:false */
+
+function graficoPeso () {
+    'use strict'
+  
+    feather.replace({ 'aria-hidden': 'true' })
+  
+    // Graphs
+    var ctx = document.getElementById('myChart')
+    // eslint-disable-next-line no-unused-vars
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: [
+          'Domingo',
+          'Segunda',
+          'Terça',
+          'Quarta',
+          'Quinta',
+          'Sexta',
+          'Sábado'
+        ],
+        datasets: [{
+          data: [
+            110,
+            108.5,
+            107.2,
+            106.8,
+            106.0,
+            106.1,
+            105.6
+          ],
+          lineTension: 0,
+          backgroundColor: 'transparent',
+          borderColor: '#007bff',
+          borderWidth: 4,
+          pointBackgroundColor: '#007bff'
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: false
+            }
+          }]
+        },
+        legend: {
+          display: false
+        }
+      }
+    })
+  }
+  
